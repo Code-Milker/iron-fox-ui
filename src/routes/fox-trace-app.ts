26c52bbdb4049099adb0f4597a001d06771bae51
Code_Milker <tylerf66@gmail.com>
2025-01-19 16:58:42 -0600

    added remaining feature list

--- src/routes/fox-trace-app.ts
+++ src/routes/fox-trace-app.ts
@@ -2,7 +2,7 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import { render, renderPage } from "../utils/moo-moo.ts";
 const router = new Router();
-router.get("/fox-trace", async (ctx) => {
+router.get("/fox-trace-app", async (ctx) => {
   const product = await getProduct();
   const features = await getFeatures();
   const pricing = await getPricing();
@@ -42,7 +42,7 @@ const getPricing = async () => {
   return content;
 };
 const getFeatures = async () => {
-  const card1 = await render(
+  const card = await render(
     join(Deno.cwd(), "src/partials", "card.html"),
     {},
     {
@@ -54,74 +54,12 @@ const getFeatures = async () => {
     "",
   );
 
-  const card2 = await render(
-    join(Deno.cwd(), "src/partials", "card.html"),
-    {},
-    {
-      title: "Mixer Decoding",
-      body: `
-    Analyzes mixed transactions to untangle scrambled trails and reveal fund origins.`,
-      extra: "",
-    },
-    "",
-  );
-
-  const card3 = await render(
-    join(Deno.cwd(), "src/partials", "card.html"),
-    {},
-    {
-      title: "Cross-Chain Tracking",
-      body: `
-    Traces assets across multiple chains, providing a seamless view of movements through bridges and swaps.`,
-      extra: "",
-    },
-    "",
-  );
-
-  const card4 = await render(
-    join(Deno.cwd(), "src/partials", "card.html"),
-    {},
-    {
-      title: "Attacker and Cash-Out Database",
-      body: `
-    Leverages a continually updated repository of known attacker wallets and cash-out addresses.`,
-      extra: "",
-    },
-    "",
-  );
-
-  const card5 = await render(
-    join(Deno.cwd(), "src/partials", "card.html"),
-    {},
-    {
-      title: "Recursive Transaction Analysis",
-      body: `
-    Uses advanced algorithms to recursively map transaction histories, identifying the flow of funds even through obfuscation layers.`,
-      extra: "",
-    },
-    "",
-  );
-
-  const card6 = await render(
-    join(Deno.cwd(), "src/partials", "card.html"),
-    {},
-    {
-      title: "Breadth-First Pathfinding",
-      body: `
-    Pinpoints critical nodes in the transaction graph, allowing precise identification of where funds are concentrated or cashed out.`,
-      extra: "",
-    },
-    "",
-  );
-
-  // cards = `${cards[0]}`;
   const content = await render(
     join(Deno.cwd(), "src/partials", "content.html"),
     {},
     {
       title: "The Algorithm That Fights Back",
-      content: [card1, card2, card3].map((c) => `<div class="mb-5">${c}</div>`)
-        .join(""),
+      content: card,
     },
     "",
   );
